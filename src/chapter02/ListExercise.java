0cbeb642736b70e1285dd7efeb4669484c80486f
ShamNeko <mkdvlp01@gmail.com>
2024-09-30 23:29:46 +0900

    リストをループで回してテスト

--- src/chapter02/ListExercise.java
+++ src/chapter02/ListExercise.java
@@ -1,10 +1,28 @@
 package chapter02;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ListExercise
+ */
 public class ListExercise {
+    /**
+     * エントリーポイント
+     * @param args
+     */
+    public static void main(String[] args) {
+        // String型を格納できるListを用意
+        List<String> names = new ArrayList<>();
+        // String 型のデータを格納する
+        names.add("太郎");
+        names.add("次郎");
+        names.add("SABUROU");
+        // 拡張for文で1データずつ表示する
+        for (String name : names) {
+            System.out.println(name);
+        }
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
+    }
 
 }
